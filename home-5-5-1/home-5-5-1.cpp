1413490ca26580e053aac9e9c9fc2ec0349f40c7
Vano <vano50@mail.ru>
2024-09-30 12:01:57 +0300

    Initial commit

---
+++ home-5-5-1/home-5-5-1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+void removeDuplicates(std::vector<int>& v) {
+    sort(v.begin(), v.end());
+    auto end_unique = unique(v.begin(), v.end());
+    v.erase(end_unique, v.end());
+}
+
+int main() {
+    std::vector<int> numbers{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]: \n";
+    for (const int& num : numbers) {
+        std::cout << num << ' ';
+    }
+    std::cout << std::endl;
+
+    removeDuplicates(numbers);
+
+    std::cout << "\n[OUT]: \n";
+    for (const int& num : numbers) {
+        std::cout << num << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
